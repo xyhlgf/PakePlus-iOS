13ce030d01393f4d924339f44ad3889bf7a40380
xyhlgf <13288276369@163.com>
2025-07-13 03:39:37 +0800

    update custom js from pakeplus

--- PakePlus/custom.js
+++ PakePlus/custom.js
@@ -4,7 +4,7 @@ console.log(
 )
 
 // very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
@@ -24,3 +24,26 @@ const hookClick = (e) => {
 }
 
 document.addEventListener('click', hookClick, { capture: true })
+
+// css filter
+document.addEventListener('DOMContentLoaded', () => {
+    const targetNode = document.body
+    // 配置观察选项
+    const config = {
+        childList: true,
+        subtree: true,
+    }
+    const observer = new MutationObserver((mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                const element0 = document.querySelector('<div class="navbar-brand"><a class="navbar-logo" href="https://hlgf001.top/index"><img src="https://hlgf001.top/index/wp-content/uploads/2025/07/扫光logo.gif" switch-src="https://hlgf001.top/index/wp-content/uploads/2025/07/扫光logo蓝色.gif" alt="欢乐工坊官方网站">
+			</a></div>');
+                if (element0) {
+                    element0.style.display = 'none';
+                }
+            }
+        }
+    })
+    observer.observe(targetNode, config)
+})
+// end css filter
